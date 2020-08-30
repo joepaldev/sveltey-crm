71cfc7341769ad65b4b24e691bde5328cc5554cd
Joe Palala <2881953+joepaldev@users.noreply.github.com>
2020-08-30 09:15:34 +0000

    Users Pages re-created
    Notes:
    - resources/js for the svelte main code
    - resources/js/Pages - one page per svelte component
    - resources/views/app.blade.php - loads @inertia (laravel blade) directive

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+require('./bootstrap');
+
+import { InertiaApp } from '@inertiajs/inertia-svelte'
+
+const app = document.getElementById('app')
+
+new InertiaApp({
+  target: app,
+  props: {
+    initialPage: JSON.parse(app.dataset.page),
+    resolveComponent: name => require(`./Pages/${name}.svelte`).default,
+  },
+})
