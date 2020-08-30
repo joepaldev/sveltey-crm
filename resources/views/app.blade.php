c75ba48ccaeed4e29c68776cad7a4079d1015483
Joe Palala <2881953+joepaldev@users.noreply.github.com>
2020-08-30 11:15:29 +0000

    renamed oldapp

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,11 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
     <script src="{{ mix('/js/app.js') }}" defer></script>
-    @routes
 </head>
 <body class="font-sans leading-none text-gray-800 antialiased">
-
-@inertia
-
+  @inertia
 </body>
 </html>
